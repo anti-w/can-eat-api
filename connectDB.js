38ad0f82ef0c3e742375521eb23bd5440ecf084a
Wendel Demetrio <hey.wendel95@gmail.com>
2021-12-29 09:05:10 -0300

    new structure to deploy API on Heroku

--- connectDB.js
+++ connectDB.js
@@ -1,5 +1,5 @@
 const fs = require('firebase-admin') //firebase admin
-const serviceAccount = require('./authService.json') //chave-key gerada no site do firebase
+const serviceAccount = require('./firebase/authService.json') //chave-key gerada no site do firebase
 
 const connectDb = fs.initializeApp({
   credential: fs.credential.cert(serviceAccount)
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const alimentosRef = firebase.firestore().collection("alimentos")
 
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 
 
