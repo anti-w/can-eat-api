94a05ab578aa640b00cae799fbb71eef55923dac
Wendel Demétrio <hey.wendel95@gmail.com>
2022-03-17 16:23:10 -0300

    connection to mongo and deploy on heroku

--- connectDB.js
+++ connectDB.js
@@ -1,22 +1,7 @@
-const fs = require('firebase-admin') //firebase admin
+const { MongoClient } = require("mongodb");
 require("dotenv").config();
 
+const uri = `mongodb+srv://${process.env.MONGODB_CREDENTIALS}@cluster0.o6yj8.mongodb.net/can-eat-db?retryWrites=true&w=majority`;
+const client = new MongoClient(uri);
 
-
-const connectDb = fs.initializeApp({
-  credential: fs.credential.cert({
-    type: process.env.FIREBASE_TYPE,
-    project_id: process.env.FIREBASE_PROJECT_ID,
-    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    client_email: process.env.FIREBASE_CLIENT_EMAIL,
-    client_id: process.env.FIREBASE_CLIENT_ID,
-    auth_uri: process.env.FIREBASE_AUTH_URI,
-    token_uri: process.env.FIREBASE_TOKEN_URI,
-    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER,
-    client_x509_cert_url: process.env.FIREBASE_CLIENT
-  })
-}) //inicializando o App do firebase com as credenciais do projeto definidas em modo teste
-
-
-module.exports = connectDb
+module.exports = client;
--- index.js
+++ index.js
@@ -1,50 +1,17 @@
-const firebase = require('./connectDB')
-const alimentosRef = firebase.firestore().collection("alimentos")
-const gruposRef = firebase.firestore().collection('grupos')
-const cors = require('cors')
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
-
-app.use(cors())
-
-
-
-app.get('/alimentos', async (req, res) => {
-
-  const snapshot = await alimentosRef.orderBy('Nome').limit(10).get()
-  const allData = []
-  snapshot.forEach(doc => {
-    allData.push(doc.data())
-  });
-  res.send(allData)
-})
-
-app.get('/groups', async (req, res) => {
-
-  const snapshot = await gruposRef.orderBy('nome').get();
-  const allData = []
-  snapshot.forEach(doc => {
-    allData.push(doc.data())
-  });
-  res.send(allData)
-})
-
-app.get('/:group', async (req, res) => {
-  const data = []
-  const groupRef = await alimentosRef.orderBy('Nome', 'asc').where('Grupo', '==', req.params.group).limit(10).get()
-
-  if (groupRef.empty) {
-    console.log('Nenhum alimento para este grupo')
-    return
-  }
-  groupRef.forEach(doc => {
-    data.push(doc.data())
-  })
-  res.send(data)
-
-})
+const connect = require("./connectDB");
+const cors = require("cors");
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+connect.connect();
+const mongoClient = connect.db("can-eat-db").collection("foods");
+app.use(cors());
 
 app.listen(port, () => {
-  console.log(`App rodando! Acesse: http://localhost:${port}`)
-})
+  console.log(`App rodando! Acesse: http://localhost:${port}`);
+});
+
+app.get("/", async (req, res) => {
+  const result = await mongoClient.find({}).toArray();
+  res.send(result);
+});
