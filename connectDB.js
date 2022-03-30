1bb3abdfdad4b4e3ec77de9dab965db1740bf5d6
Wendel Demétrio <hey.wendel95@gmail.com>
2022-03-30 17:04:51 -0300

    instaled mongoose

--- connectDB.js
+++ connectDB.js
@@ -1,7 +1,13 @@
-const { MongoClient } = require("mongodb");
+const mongoose = require("mongoose");
+
 require("dotenv").config();
 
 const uri = `mongodb+srv://${process.env.MONGODB_CREDENTIALS}@cluster0.o6yj8.mongodb.net/can-eat-db?retryWrites=true&w=majority`;
-const client = new MongoClient(uri);
+const client = mongoose
+  .connect(uri)
+  .then(() => {
+    console.log("Connection established");
+  })
+  .catch((err) => console.log(err));
 
 module.exports = client;
--- index.js
+++ index.js
@@ -1,15 +1,39 @@
 const connect = require("./connectDB");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const cors = require("cors");
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
-connect.connect();
-const mongoClient = connect.db("can-eat-db").collection("foods");
-app.use(cors());
+
+//Config JSON response
+app.use(express.json());
 
 app.listen(port, () => {
   console.log(`App rodando! Acesse: http://localhost:${port}`);
 });
+
+//Models
+
+const User = require("./models/User");
+
+//Register User
+app.post("/auth/register", async (req, res) => {
+  const { name, email, password, confirmpassword } = req.body;
+
+  //validation
+  if (!name) {
+    return res.status(422).json({ msg: "O nome é obrigatório mané" });
+  }
+  if (!email) {
+    return res.status(422).json({ msg: "O e-mail é obrigatório mané" });
+  }
+  if (!password) {
+    return res.status(422).json({ msg: "O password é obrigatório mané" });
+  }
+});
+
+//Public Route
 
 app.get("/", async (req, res) => {
   const result = await mongoClient.find({}).toArray();
