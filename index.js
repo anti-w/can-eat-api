7b2c738ed4fcd8ba666048119bd0907cfa1d4eb2
Wendel Demétrio <hey.wendel95@gmail.com>
2022-04-01 10:37:13 -0300

    fixed errors with cors

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors);
+app.use(cors());
 //Config JSON response
 app.use(express.json());
 
